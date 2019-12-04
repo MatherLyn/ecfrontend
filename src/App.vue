11c349be61dbb3c0b0512517092a64a459d33203
MatherLyn <2907681146@qq.com>
2019-12-04 19:40:36 +0800

    new pages

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,79 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container style="height: 100vh; border: 1px solid #eee">
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+          <el-menu router :default-openeds="['1', '3']">
+            <el-menu-item index="/orderlist">
+              <template slot="title"><i class="el-icon-document-checked"></i>订单管理</template>
+            </el-menu-item>
+            <el-menu-item index="/settings">
+              <template slot="title"><i class="el-icon-setting"></i>个人设置</template>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+
+      <el-container>
+        <el-header style="text-align: right; font-size: 12px">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>查看</el-dropdown-item>
+              <el-dropdown-item>新增</el-dropdown-item>
+              <el-dropdown-item>删除</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <span>王小虎</span>
+        </el-header>
+        <el-main>
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+  }
+
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    color: #333;
+  }
+
+  .el-main {
+    text-align: left;
+  }
+
+  .el-col {
+    text-align: center;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Settings from '../views/Settings'
+import OrderList from '../views/OrderList'
 
 Vue.use(VueRouter)
 
@@ -8,15 +9,17 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    redirect: 'orderlist'
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/orderlist',
+    name: 'orderlist',
+    component: OrderList
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: Settings,
   }
 ]
 
